c38bf853f8e80a6937857b38216fc427e7442953
garetjax <garetjax@web31.webfaction.com>
2010-08-26 15:56:18 -0500

    Documentation update

--- _static/ie8.js
+++
@@ -1,6 +0,0 @@
-document.createElement('header');
-document.createElement('section');
-document.createElement('nav');
-document.createElement('footer');
-document.createElement('time');
-document.createElement('aside');
